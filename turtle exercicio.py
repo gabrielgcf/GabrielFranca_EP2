849a359af12190d156f547142d1c63297fa115db
gabrielgcf <gabrielgcf@al.insper.edu.br>
2015-03-24 21:38:03 -0300

    Inicio desenho forca

---
+++ turtle exercicio.py
@@ -0,0 +1,18 @@
+
+
+
+import turtle
+screen = turtle.Screen() #abrir aba turtle
+
+x = screen.bgcolor("blue") #cor do fundo
+screen.title("JOGO DA FORCA")  #nome da janela
+
+
+
+turtle.pos(150,-150)    #posiçao inicial
+turtle.back(150)
+turtle.left(90)
+turtle.fd(250)
+
+
+exitonclick()   #fechar
